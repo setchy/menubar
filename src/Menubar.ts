0d769ba3345ce8e41e36c686abb35befb2a347e1
Adam Setch <adam.setch@outlook.com>
2026-07-30 08:57:06 -0400

    chore: update biomejs and lint codebase
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/Menubar.ts
+++ src/Menubar.ts
@@ -1,6 +1,6 @@
-import { EventEmitter } from 'events';
-import fs from 'fs';
-import path from 'path';
+import { EventEmitter } from 'node:events';
+import fs from 'node:fs';
+import path from 'node:path';
 import { BrowserWindow, Tray } from 'electron';
 import Positioner from 'electron-positioner';
 
@@ -160,7 +160,7 @@ export class Menubar extends EventEmitter {
     }
 
     // Default the window to the right if `trayPos` bounds are undefined or null.
-    let noBoundsPosition = undefined;
+    let noBoundsPosition: Options['windowPosition'];
     if (
       (trayPos === undefined || trayPos.x === 0) &&
       this._options.windowPosition &&
